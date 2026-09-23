6915ea7ddc16c980726cc7ae3293a797e29157ea
ermwhathesigma656 <182420889+ermwhathesigma656@users.noreply.github.com>
2026-09-23 16:54:31 -0500

    Personalize the random home screen greetings

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -50,18 +50,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox") && store.get("ab") === 
 }
 
 // Splash texts
-const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
-];
+const SplashT = ["Hello T3", "Hello Mr I"];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
 const SplashE = document.getElementById("splash");
